8fd900324a89e6124a17e2cd843e30df470237fa
ShravanShinde01 <shinde.swe.3045@gmail.com>
2026-09-07 09:54:44 +0530

    Add Maven Spring Boot project

---
+++ v1-Maven/mvnPro/src/main/java/com/first_Project/mvnPro/MvnProApplication.java
@@ -0,0 +1,13 @@
+package com.first_Project.mvnPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnProApplication.class, args);
+	}
+
+}
---
+++ v1-Maven/mvnPro/src/test/java/com/first_Project/mvnPro/MvnProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first_Project.mvnPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvnProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
